b69918f5707828627cab302b9e4dac77848b90e2
nicod12 <dumenicolas45@gmail.com>
2024-06-11 12:37:18 -0300

    upd

--- src/utils/userHandlers.ts
+++ src/utils/userHandlers.ts
@@ -7,95 +7,83 @@ const UserSchema = new mongoose.Schema({
     password: { type: String, required: true }
 });
 
-const CORS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, DELETE',
-    'Access-Control-Allow-Headers': 'Content-Type',
-};
 
 
 const UserModel = mongoose.model("User", UserSchema);
 
 
-export const handleGetAllUsers  = async  () => {
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+    'Access-Control-Allow-Headers': '*',
+};
+
+
+export const handleGetAllUsers = async () => {
     const users = await UserModel.find({});
     return new Response(JSON.stringify(users), {
-        headers: {
-            'Content-Type': 'application/json',
-            ...CORS_HEADERS
-        }
+        status: 200,
+        headers: corsHeaders,
     });
 }
 
-export const handleGetUserById  = async  (id: string) => {
+export const handleGetUserById = async (id: string) => {
     const user = await UserModel.findById(id);
 
     if (!user) {
         return new Response('User not found', {
             status: 404,
-            headers: {
-                ...CORS_HEADERS
-            }
+            headers: corsHeaders,
         });
     }
 
     return new Response(
         JSON.stringify(user), {
-            headers: { 'Content-Type': 'application/json',
-                ...CORS_HEADERS
-            },
-    });
+            status: 200,
+            headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        });
 }
 
-export const handleCreateUser = async  (username: string, email: string, password: string) => {
+export const handleCreateUser = async (username: string, email: string, password: string) => {
     const newUser = new UserModel({ username, email, password });
 
     await newUser.save();
 
     return new Response(JSON.stringify(newUser), {
         status: 201,
-        headers: { 'Content-Type': 'application/json',
-            ...CORS_HEADERS
-         },
+        headers: corsHeaders,
     });
 }
+
 export const handleUpdateUser = async (id: string, username: string, email: string, password: string) => {
     const user = await UserModel.findByIdAndUpdate(id, { username, email, password }, { new: true });
 
     if (!user) {
         return new Response("User Not Found", {
             status: 404,
-            headers: { 'Content-Type': 'application/json',
-                ...CORS_HEADERS
-             },
+            headers: corsHeaders,
         });
     }
 
     return new Response("User Updated", {
         status: 200,
-        headers: {
-            ...CORS_HEADERS
-        }
+        headers: corsHeaders,
     });
-
 }
 
-export const handleDeleteUser = async  (id: string) => {
+export const handleDeleteUser = async (id: string) => {
     const user = await UserModel.findByIdAndDelete(id);
 
     if (!user) {
         return new Response("User Not Found", {
             status: 404,
-            headers: {
-                ...CORS_HEADERS
-            }
+            headers: corsHeaders,
         });
     }
 
     return new Response("User Deleted", {
         status: 200,
-        headers: {
-            ...CORS_HEADERS
-        }
+        headers: corsHeaders,
     });
 }
